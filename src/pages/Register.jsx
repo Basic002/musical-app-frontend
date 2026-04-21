307484273e8b846a5e4a8d1e00c9a96a3cde88cc
unknown <enzo.giordani57@gmail.com>
2026-04-21 13:11:22 +0200

    Page register terminée

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -66,17 +66,32 @@ const Register = () => {
             />
           </div>
           {/* Champ Mot de passe */}
-            <div>
-                <label className="block text-sm front-medium text-night-text mb-1">
-                    Mot de passe*
-                </label>
-                <input
-                    type="password"
-                    placeholder="••••••••"
-                    className="w-full bg-night-bg border border-slate-700 rounded-lg px-4 py-3 text-white focus:outline-none focus:border-neon-violet transition-colors placeholder-slate-600"
-                />
-            </div>
+          <div>
+            <label className="block text-sm front-medium text-night-text mb-1">
+              Mot de passe*
+            </label>
+            <input
+              type="password"
+              placeholder="••••••••"
+              className="w-full bg-night-bg border border-slate-700 rounded-lg px-4 py-3 text-white focus:outline-none focus:border-neon-violet transition-colors placeholder-slate-600"
+            />
+          </div>
+          {/* Bouton de validation */}
+          <button
+            type="submit"
+            className="w-full bg-neon-violet hover:bg-purple-500 text-white font-title font-bold py-3 rounded-lg mt-6 transition-all shadow-[0_0_15px_rgba(139,92,246,0.3)] hover:shadow-[0_0_20px_rgba(139,92,246,0.5)]"
+          >
+            S'inscrire
+          </button>
         </form>
+
+        {/* Bouton de validation */}
+        <button
+          type="submit"
+          className="w-full bg-neon-violet hover:bg-purple-500 text-white font-title font-bold py-3 rounded-lg mt-6 transition-all shadow-[0_0_15px_rgba(139,92,246,0.3)] hover:shadow-[0_0_20px_rgba(139,92,246,0.5)]"
+        >
+          S'inscrire
+        </button>
       </div>
     </div>
   );
